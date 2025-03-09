171892c06ffc8e22faf0d48c751d666a6ccacddf
moreniekmeijer <niekjmeijer@gmail.com>
2025-03-09 21:04:00 +0100

    Added all basic methods to controllers and services

--- src/main/java/nl/moreniekmeijer/backendspringboottechiteasycontroller/repositories/WallBracketRepository.java
+++ src/main/java/nl/moreniekmeijer/backendspringboottechiteasycontroller/repositories/WallBracketRepository.java
@@ -3,6 +3,5 @@ package nl.moreniekmeijer.backendspringboottechiteasycontroller.repositories;
 import nl.moreniekmeijer.backendspringboottechiteasycontroller.models.WallBracket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WallBracketRepository extends JpaRepository<WallBracket, Integer> {
-
+public interface WallBracketRepository extends JpaRepository<WallBracket, Long> {
 }
